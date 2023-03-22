16026b0019e9b34ae1a553f3681205fba9e5630f
arenanlira <128628734+arenanlira@users.noreply.github.com>
2023-03-22 14:19:15 -0300

    Upload do projeto
    Projeto completo de acordo com as especificações do cliente.

---
+++ Cartorio.c
@@ -0,0 +1,160 @@
+#include <stdio.h> //biblioteca de comunicação com o usuário
+#include <stdlib.h> //biblioteca de alocação de espaço em memória
+#include <locale.h> //bibilioteca de alocações de texto por região
+#include <string.h> //biblioteca responsável por cuidar das strings
+
+
+int registro() //Função responsável por cadastrar os usuários no sistema
+{
+      //Inicio da criação de variáveis/string
+      char arquivo[40];
+      char cpf[40];
+      char nome[40];
+      char sobrenome[40];
+      char cargo[40];
+      //final da criação de strings/variáveis
+
+      printf("Digite o CPF a ser cadastrado: "); //coletando informações do usuário
+      scanf("%s", cpf); //%s referese-a string
+
+      strcpy(arquivo, cpf); //Responsável por copiar o valor das strings
+
+      FILE *file; //Cria o arquivo
+      file = fopen(arquivo, "w"); //Cria o arquivo e o "w" significa write
+      fprintf(file,cpf); //Salva o valor da variável
+      fclose(file); //Fecha o arquivo
+
+      file = fopen(arquivo, "a");
+      fprintf(file,",");
+      fclose(file);
+
+
+      printf("Digite o nome a ser cadastrado: ");
+      scanf("%s,",nome);
+
+      file = fopen(arquivo, "a");
+      fprintf(file,nome);
+      fclose(file);
+
+      file = fopen(arquivo, "a");
+      fprintf(file,",");
+      fclose(file);
+
+      printf("Digite o sobrenome a ser cadastrado: ");
+      scanf("%s",sobrenome);
+
+      file = fopen(arquivo, "a");
+      fprintf(file,sobrenome);
+      fclose(file);
+
+      file = fopen(arquivo, "a");
+      fprintf(file,",");
+      fclose(file);
+
+      printf("Digite o cargo a ser cadastrado: ");
+      scanf("%s",cargo);
+
+      file = fopen(arquivo, "a");
+      fprintf(file,cargo);
+      fclose(file);
+
+      getchar();
+
+}
+
+int consulta()
+{
+
+      setlocale(LC_ALL, "Portuguese"); // Definindo a linguagem
+
+      char cpf[40];
+      char conteudo [200];
+
+      printf("Digite o CPF a ser consultado: ");
+      scanf("%s",cpf);
+
+      FILE *file;
+      file = fopen(cpf, "r");
+
+      if(file == NULL)
+      {
+            printf("Não foi possível abrir o arquivo, não localizado!\n");
+      }
+
+      while(fgets(conteudo,200, file) != NULL)
+      {
+            printf("\nEstas são as informações do usuário.");
+            printf("%s", conteudo);
+            printf("\n\n");
+      }
+
+      getchar();
+}
+
+int deletar()
+{
+      char cpf[40];
+
+      printf("Insira o CPF do usuário a ser deletado: ");
+      scanf("%s",cpf);
+
+      remove(cpf);
+
+      FILE *file;
+      file = fopen(cpf,"r");
+
+      if(file == NULL)
+      {
+            printf("O usuário não se encontra no sistema!\n");
+            getchar();
+      }
+
+}
+
+int main ()
+{
+
+      int opcao=0; //Definindo as variáveis
+      int laco=1;
+
+      for (laco=1;laco=1;)
+      {
+
+            system("clear");
+
+            setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
+
+            printf("### Cartório da Ebac ###\n\n"); //Inicio do menu
+            printf("\t1 - Registrar nomes\n");
+            printf("\t2 - Consultar nomes\n");
+            printf("\t3 - Deletar nomes\n\n"); //Fim do menu
+            printf("Opção:");
+
+            scanf("%d",&opcao); //armazenando aescolha do usuario
+
+            system("clear"); //responsável por limpar a tela
+
+            switch(opcao) //inicio da seleção do menu
+            {
+                  case 1:
+                  registro(); //chamada de funções
+                  getchar();
+                  break;
+
+                  case 2:
+                  consulta();
+                  getchar();
+                  break;
+
+                  case 3:
+                  deletar();
+                  getchar();
+                  break;
+
+                  default:
+                  printf("Este comando não existe.");
+                  getchar();
+                  break;
+            }
+      }
+}
